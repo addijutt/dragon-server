8e7169afa90fa309ac31a9df5c205e8f5245ee03
addijutt <dilawarjutt44@gmail.com>
2025-09-03 03:37:25 +0500

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173', 'https://mixmorph-dragon-demo.vercel.app' // <-- replace with your frontend URL
+      ],
+      headers: '*',  // allow all headers
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
